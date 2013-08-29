9d2e1c175dc720d87957ef0eed654ae58606a2a7
Boudewijn <boudewijn-tribler@frayja.com>
2013-08-29 09:49:29 +0200

    Jenkins specific test.

--- evaluation/99-all.sh
+++ evaluation/99-all.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
-# echo on
-set -o verbose
-
 # directory with all the scripts
 EVAL=`dirname $0`
 
 # ARGUMENT $1: directory with all the logs, default '.'
 RESULTDIR=${1:-.}
 
-# ARGUMENT $2: database filename, default 'try.db'
-DATABASE=${2:-try.db}
-rm -f "${DATABASE}"
-
 # source config file
-source ${RESULTDIR}/config
+if [ ! -f "${RESULTDIR}/config" ]; then
+    echo "${RESULTDIR} does not contain config file"
+    exit 1
+fi
+cat "${RESULTDIR}/config"
+source "${RESULTDIR}/config"
+
+# ARGUMENT $2: database filename, default 'try.db'
+DATABASE=${2:-${FILENAME_PREFIX}try.db}
+
+echo "TEST FROM 99-all.sh FILENAME_PREFIX: ${FILENAME_PREFIX}"
+echo "TEST FROM 99-all.sh TITLE_POSTFIX: ${TITLE_POSTFIX}"
+echo "TEST FROM 99-all.sh TOTAL_MESSAGE_COUNT: ${TOTAL_MESSAGE_COUNT}"
+cat $EVAL/test.R | R --no-save --quiet || exit 1
+
+# echo on
+set -o verbose
 
 # run all scripts
-$EVAL/11-parse.py $RESULTDIR log $DATABASE || exit 1
-$EVAL/12-normalize.py $DATABASE || exit 1
-$EVAL/13-dissemination.py $DATABASE || exit 1
-cat $EVAL/21-graphs.R | sed s:==FILENAME==:$DATABASE: | R --no-save --quiet || exit 1
+# $EVAL/11-parse.py $RESULTDIR log $DATABASE || exit 1
+# $EVAL/12-normalize.py $DATABASE || exit 1
+# $EVAL/13-dissemination.py $DATABASE || exit 1
+# cat $EVAL/21-graphs.R | sed s:==FILENAME==:$DATABASE: | R --no-save --quiet || exit 1
 
 # find . -type f -name dispersy.db -exec sqlite3 -noheader -separator ' ' {} "select meta_message.name, count(*) from sync join meta_message on meta_message.id = sync.meta_message group by sync.meta_message" \; > messages_in_database
