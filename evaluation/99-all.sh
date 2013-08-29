d4e9f7763659dba7139b8b996df19ae5138a3711
Boudewijn <boudewijn-tribler@frayja.com>
2013-08-29 10:05:48 +0200

    Removed Jenkins specific tests and included export

--- evaluation/99-all.sh
+++ evaluation/99-all.sh
@@ -16,21 +16,13 @@ source "${RESULTDIR}/config"
 # ARGUMENT $2: database filename, default 'try.db'
 DATABASE=${2:-${FILENAME_PREFIX}try.db}
 
-FILENAME_PREFIX="SET-VALUE-TEST"
-export TITLE_POSTFIX="EXPORT-VALUE-TEST"
-
-echo "TEST FROM 99-all.sh FILENAME_PREFIX: ${FILENAME_PREFIX}"
-echo "TEST FROM 99-all.sh TITLE_POSTFIX: ${TITLE_POSTFIX}"
-echo "TEST FROM 99-all.sh TOTAL_MESSAGE_COUNT: ${TOTAL_MESSAGE_COUNT}"
-cat $EVAL/test.R | R --no-save --quiet || exit 1
-
 # echo on
 set -o verbose
 
 # run all scripts
-# $EVAL/11-parse.py $RESULTDIR log $DATABASE || exit 1
-# $EVAL/12-normalize.py $DATABASE || exit 1
-# $EVAL/13-dissemination.py $DATABASE || exit 1
-# cat $EVAL/21-graphs.R | sed s:==FILENAME==:$DATABASE: | R --no-save --quiet || exit 1
+$EVAL/11-parse.py $RESULTDIR log $DATABASE || exit 1
+$EVAL/12-normalize.py $DATABASE || exit 1
+$EVAL/13-dissemination.py $DATABASE || exit 1
+cat $EVAL/21-graphs.R | sed s:==FILENAME==:$DATABASE: | R --no-save --quiet || exit 1
 
 # find . -type f -name dispersy.db -exec sqlite3 -noheader -separator ' ' {} "select meta_message.name, count(*) from sync join meta_message on meta_message.id = sync.meta_message group by sync.meta_message" \; > messages_in_database
--- evaluation/test.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env R
-
-filename_prefix <- Sys.getenv("FILENAME_PREFIX")
-title_postfix <- Sys.getenv("TITLE_POSTFIX")
-total_message_count <- as.numeric(Sys.getenv("TOTAL_MESSAGE_COUNT"))
-
-filename_prefix
-title_postfix
-total_message_count
