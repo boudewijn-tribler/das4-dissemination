4444c60819da93c7efdb795ce4b92c4503d01eae
Boudewijn <boudewijn-tribler@frayja.com>
2013-08-29 11:13:36 +0200

    Adds pretty subtitle to graphs.

--- evaluation/21-graphs.R
+++ evaluation/21-graphs.R
@@ -17,7 +17,12 @@ total_message_count <- as.numeric(Sys.getenv("TOTAL_MESSAGE_COUNT"))
 
 # database connection
 con <- dbConnect("SQLite", dbname="==FILENAME==")
-#con <- dbConnect("SQLite", dbname="~/remote/scratch/pbschoon/resultdir-dissemination-random/try.db")
+
+# dummy data
+#filename_prefix <- "FILENAME_PREFIX"
+#title_postfix <- "Dummy title postfix [foo, bar, moo, milk]"
+#total_message_count <- 3000
+#con <- dbConnect("SQLite", dbname="~/remote/scratch/pbschoon/resultdir/try.db")
 
 # CPU
 res <- dbSendQuery(con, statement=paste("SELECT peer.hostname AS hostname, cpu.timestamp AS timestamp, cpu.percentage / 100.0 AS percentage FROM cpu JOIN peer ON peer.id = cpu.peer"))
@@ -25,11 +30,13 @@ DATA <- data.frame(fetch(res, n=-1))
 sqliteCloseResult(res)
 
 p <- ggplot(DATA, aes(timestamp, percentage, color=hostname))
-p <- p + labs(title=paste("CPU usage", title_postfix), x="Time (seconds)", y="Usage (percentage)")
+p <- p + labs(title=bquote(atop("CPU usage", atop(italic(.(title_postfix))))))
+p <- p + labs(x="Time (seconds)", y="Usage (percentage)")
 p <- p + geom_point(shape=".")
 p <- p + geom_smooth(method="auto")
 p <- p + scale_y_continuous(labels=percent)
-ggsave(filename=paste(filename_prefix, "cpu.png", sep=""), plot=p)
+p
+ggsave(filename=paste(filename_prefix, "cpu.png", sep=""))
 
 # memory
 res <- dbSendQuery(con, statement=paste("SELECT timestamp, rss, vms FROM memory"))
@@ -37,11 +44,13 @@ DATA <- data.frame(fetch(res, n=-1))
 sqliteCloseResult(res)
 
 p <- ggplot(DATA, aes(timestamp, vms))
-p <- p + labs(title=paste("Virtual Memory Size", title_postfix), x="Time (seconds)", y="Usage (bytes)")
+p <- p + labs(title=bquote(atop("Virtual Memory Size", atop(italic(.(title_postfix))))))
+p <- p + labs(x="Time (seconds)", y="Usage (bytes)")
 p <- p + geom_point(shape=".")
 p <- p + geom_smooth(method="auto")
 p <- p + scale_y_continuous(labels=comma)
-ggsave(filename=paste(filename_prefix, "memory.png", sep=""), plot=p)
+p
+ggsave(filename=paste(filename_prefix, "memory.png", sep=""))
 
 # bandwidth
 res <- dbSendQuery(con, statement=paste("SELECT timestamp, up, down FROM bandwidth_rate"))
@@ -50,11 +59,13 @@ DATA <- melt(DATA, id=c("timestamp"))
 sqliteCloseResult(res)
 
 p <- ggplot(DATA, aes(timestamp, value, color=variable))
-p <- p + labs(title=paste("Bandwidth rate", title_postfix), x="Time (seconds)", y="Usage (bytes)")
+p <- p + labs(title=bquote(atop("Bandwidth rate", atop(italic(.(title_postfix))))))
+p <- p + labs(x="Time (seconds)", y="Usage (bytes)")
 p <- p + geom_point(shape=".")
 p <- p + geom_smooth(method="auto")
 p <- p + scale_y_continuous(labels=comma)
-ggsave(filename=paste(filename_prefix, "bandwidth.png", sep=""), plot=p)
+p
+ggsave(filename=paste(filename_prefix, "bandwidth.png", sep=""))
 
 # packet success and drop
 res <- dbSendQuery(con, statement=paste("SELECT timestamp, drop_count, delay_count, delay_send, delay_success, delay_timeout, success_count, received_count FROM bandwidth"))
@@ -63,11 +74,13 @@ DATA <- melt(DATA, id=c("timestamp"))
 sqliteCloseResult(res)
 
 p <- ggplot(DATA, aes(timestamp, value, color=variable))
-p <- p + labs(title=paste("Ability to process received packets", title_postfix), x="Time (seconds)", y="Counters")
+p <- p + labs(title=bquote(atop("Ability to process received packets", atop(italic(.(title_postfix))))))
+p <- p + labs(x="Time (seconds)", y="Counters")
 p <- p + geom_point(shape=".")
 p <- p + geom_smooth(method="auto")
 p <- p + scale_y_continuous(labels=comma)
-ggsave(filename=paste(filename_prefix, "success_and_loss.png", sep=""), plot=p)
+p
+ggsave(filename=paste(filename_prefix, "success_and_loss.png", sep=""))
 
 # dissemination
 res <- dbSendQuery(con, statement="SELECT timestamp, peer FROM received ORDER BY timestamp")
@@ -76,11 +89,13 @@ DATA <- within(DATA, { received <- ave(timestamp, peer, FUN=seq)})
 sqliteCloseResult(res)
 
 p <- ggplot(DATA)
-p <- p + labs(title=paste("Download progress", title_postfix), x="Time (seconds)", y="Records received")
+p <- p + labs(title=bquote(atop("Download progress", atop(italic(.(title_postfix))))))
+p <- p + labs(x="Time (seconds)", y="Records received")
 p <- p + annotate("segment", x=0, y=0, xend=total_message_count/(27/5), yend=total_message_count)
 p <- p + geom_step(aes(x=timestamp, y=received, color=peer))
 p <- p + scale_y_continuous(labels=comma)
-ggsave(filename=paste(filename_prefix, "download_progress.png", sep=""), plot=p)
+p
+ggsave(filename=paste(filename_prefix, "download_progress.png", sep=""))
 
 # quit
 quit(save="no")
